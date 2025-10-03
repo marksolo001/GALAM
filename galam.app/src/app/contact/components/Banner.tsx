ac7293617c0fa0fd727c53fafcb85420be27f13c
symbat-s <seidahmetovberik1983@github.com>
2025-10-04 01:02:31 +0500

    new

---
+++ galam.app/src/app/contact/components/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner(){
+    return(
+        <div>
+            <h1>Hello its contact</h1>
+        </div>
+    )
+}
---
+++ galam.app/src/app/game/components/Banner.tsx
@@ -0,0 +1,10 @@
+export default function Banner() {
+    return(
+        <div>
+          <div className="w-full h-186  bg-[url('/images/game.png')] bg-cover flex items-center justify-center">
+            
+          </div>
+        </div>
+        
+    )
+}
