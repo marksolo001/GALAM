7b0a85a1c72c8f63f8a42af6819d14901bd8a3d7
symbat-s <seidahmetovberik1983@github.com>
2025-10-04 08:18:47 +0500

    new

--- galam.app/src/app/game/components/Banner.tsx
+++ galam.app/src/app/game/components/Banner.tsx
@@ -1,9 +1,26 @@
 export default function Banner() {
     return(
-        <div>
-          <div className="w-full h-186  bg-[url('/images/game.png')] bg-cover flex items-center justify-center">
-            
+        <div className="w-full h-186 pt-40 bg-[url('/images/game.png')] bg-cover">
+          <div className=" flex items-center gap-50 justify-center">
+            <div className="w-60 h-70 bg-[red] rounded-[5%] ">
+
+            </div>
+
+            <div className="w-80 h-90 bg-[red] rounded-[5%]">
+
+            </div>
+
+            <div className="w-60 h-70 bg-[red] rounded-[5%]">
+
+            </div>
+
           </div>
+
+          <div className="block text-white">
+            <button className="text-center w-15 h-5">DOWNLOAD FOR PC</button>
+            <button>DOWNLOAD FOR ANDROID</button>
+          </div>
+
         </div>
         
     )
